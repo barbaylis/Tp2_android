660eaf19e408cba44ea20446b15bf97a9ce8f9db
barbaylis <maylis.montani@gmail.com>
2020-04-12 15:13:51 +0200

    ajout comportement click share bouton et star bouton: log id + changement d'image pour star

--- app/src/main/java/m/tp2_chucknorrisjokes/JokeAdapter.kt
+++ app/src/main/java/m/tp2_chucknorrisjokes/JokeAdapter.kt
@@ -1,22 +1,32 @@
 package m.tp2_chucknorrisjokes
 
+import android.content.ContentValues.TAG
+import android.graphics.drawable.Drawable
+import android.util.Log
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class JokeAdapter(var listJokes: MutableList<Joke>, val OnBottomReached: () -> Unit) :
-        RecyclerView.Adapter<JokeAdapter.JokeViewHolder>()
-{
+    RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
+
     class JokeViewHolder(val jokeView: JokeView) : RecyclerView.ViewHolder(jokeView)
+
+    // Logs of shared Jokes
+    var logs_share : MutableList<String> =mutableListOf()
+    //Logs of star Jokes
+    var logs_star : MutableList<String> =mutableListOf()
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
         val jokeViewCreated = JokeView(parent.context)
 
         // reapplied layout param because the inflate in JokeView doesn't take account of parent
         val lp = RecyclerView.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
         )
         jokeViewCreated.setLayoutParams(lp)
 
@@ -28,17 +38,49 @@ class JokeAdapter(var listJokes: MutableList<Joke>, val OnBottomReached: () -> U
     }
 
     override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
-        if (position==this.itemCount-1)
-        {OnBottomReached()}
+        if (position == this.itemCount - 1) {
+            OnBottomReached()
+        }
 
-        //recovery of the views'components for the new model
-        val newT =holder.jokeView.findViewById(R.id.joke_layout) as TextView
-        newT.text=listJokes[position].value
-        val newStartB =holder.jokeView.findViewById(R.id.star_button) as ImageButton
-        val newShareB =holder.jokeView.findViewById(R.id.share_button) as ImageButton
-        val newModel = JokeView.Model(newT,newShareB,newStartB)
+        /////Creation of the model////
 
-        holder.jokeView.setupView(newModel)
+        //TextView with the newJoke
+        val newT = holder.jokeView.findViewById(R.id.joke_layout) as TextView
+        newT.text = listJokes[position].value
+
+        //star button behavior
+        val newStarB = holder.jokeView.findViewById(R.id.star_button) as ImageButton
+        newStarB.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View)
+            {
+                if (logs_star.contains(listJokes[position].id)) //already clicked
+                {
+                    logs_star.remove(listJokes[position].id)//remove from the star jokes list
+                    newStarB.setImageResource(R.drawable.star_softpink_notpressed) // put image star border
+                }
+
+                else //not already clicked
+                {
+                    logs_star.add(listJokes[position].id) //add the id of the joke in logs_star list
+                    newStarB.setImageResource(R.drawable.star_softpink) // put image full star
+
+                }
+            }
+        })
+
+
+        //share button behavior
+        val newShareB = holder.jokeView.findViewById(R.id.share_button) as ImageButton
+        newShareB.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View)
+            {
+                logs_share.add(listJokes[position].id) //add the id of the joke in logs_share list
+            }
+        })
+
+        val newModel = JokeView.Model(newT, newShareB, newStarB)
+
+        holder.jokeView.setupView(newModel) // send the model to update the view
     }
 
     fun addJoke(joke: Joke) {
